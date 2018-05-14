40706b28f8fd03e82b1d7cf733f1fcc33787ba5a
Elvis B. Martins <elvisbmartins@gmail.com>
2018-05-14 17:58:53 -0300

    script filtro trabalho home

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,19 @@
 /**
  * scripts.js
  */
-$( document ).ready(function() {
+(function($) {
+	$( document ).ready(function() {
 
-});
+		$('.btn-filtro').on('click', function(e){
+			e.preventDefault();
+			var classe = $(this).attr('data-class');
+			if( classe == "todos" ){
+				$('.item-trabalho').show(100);
+			}else{
+				$('.item-trabalho').hide(100);
+				$('.item-trabalho.'+classe).show(100);
+			}
+		});
+		
+	});
+})(jQuery);
