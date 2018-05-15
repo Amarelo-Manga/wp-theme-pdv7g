30166277ebee2aab98d008c25812a48cbbf21cdf
Elvis B. Martins <elvisbmartins@gmail.com>
2018-05-15 16:05:23 -0300

    estrutura footer

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
  * @package Neat
  */
 ?>
-<footer class="aa_footer">
+<footer>
 	<div class="container">
 		<div class="row">
 			<div id="logo" class="col-sm-8 col-md-4 col-lg-4 col-xl-3">
@@ -15,14 +15,14 @@
 					$custom_logo_id = get_theme_mod( 'custom_logo' );
 					$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 					if ( has_custom_logo() ) {
-					        echo '<h1><a href="'.get_home_url().'" title="'.get_bloginfo( 'name' ).'" alt="'.get_bloginfo( 'name' ).'"><img src="'. esc_url( $logo[0] ) .'" title="'.get_bloginfo( 'name' ).'" alt="'.get_bloginfo( 'name' ).'"></a> </h1>';
+					        echo '<h2><a href="'.get_home_url().'" title="'.get_bloginfo( 'name' ).'" alt="'.get_bloginfo( 'name' ).'"><img src="'. esc_url( $logo[0] ) .'" title="'.get_bloginfo( 'name' ).'" alt="'.get_bloginfo( 'name' ).'"></a> </h2>';
 					} else {
 					        echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
 					}
 				?>
 			</div>
 
-			<div class="col-sm-4 col-md-4 col-lg-4 col-xl-7 remove-padding">
+			<div class="col-sm-4 col-md-4 col-lg-4 col-xl-6 remove-padding">
 				<nav class="navbar navbar-expand-xl navbar-light">
 					<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				    	<span class="navbar-toggler-icon"></span>
@@ -45,10 +45,10 @@
 					</div>
 				</nav>
 			</div>
-			<div id="social" class="widget-top col-sm-12 col-md-4 col-lg-4 col-xl-2 remove-padding">
+			<div id="newsletter" class="widget-top col-sm-12 col-md-4 col-lg-4 col-xl-3 remove-padding">
 				<?php 
-					if ( is_active_sidebar( 'header-top' ) ) {
-						dynamic_sidebar( 'header-top' );
+					if ( is_active_sidebar( 'footer_newsletter' ) ) {
+						dynamic_sidebar( 'footer_newsletter' );
 					};
 				?>
 			</div>
