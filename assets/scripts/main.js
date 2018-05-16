5f189049348ed08f4d5281b0ebd9000d9d5103cc
Elvis B. Martins <elvisbmartins@gmail.com>
2018-05-16 12:18:02 -0300

    script modal next, prev for boostrap modal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,5 +17,39 @@
 			}
 		});
 
+		/*
+		 * Modal Trabalho 
+		 * Next e Prev
+		 */
+		 var contTrabalhos = $('.thumbnail').length;
+		$('.thumbnail').on('click',function(){
+		    $('.modal-body').empty();
+		    var title = $(this).attr("title");
+		    $('.modal-title').html(title);
+		    $($(this).parents('div').html()).appendTo('.modal-body');
+		    $('#myModal').modal({show:true});
+		});
+
+		$('#next-btn').on('click',function() {
+		    var number = $('.modal-body a').attr('data-modal');
+		    console.log(number);
+		    console.log( contTrabalhos );
+		    if( number < contTrabalhos ) {
+		    	number++;
+		    	$('.modal-body').html( $('a[data-modal="'+number+'"]').parents('div').html() );
+		    	$('.modal-title').text( $('a[data-modal="'+number+'"]').attr('title') );
+		    }
+		});
+
+		$('#prev-btn').click(function() {
+		    var number = $('.modal-body a').attr('data-modal');
+		    console.log(number);
+		    console.log( contTrabalhos );
+		    if( number <= contTrabalhos && number > 1 ) {
+		    	number--;
+		    	$('.modal-body').html( $('a[data-modal="'+number+'"]').parents('div').html() );
+		    	$('.modal-title').text( $('a[data-modal="'+number+'"]').attr('title') );
+		    }
+		});
 	});
 })(jQuery);
